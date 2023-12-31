ce02bb0bd27e17293232eef861f8756b6793e07e
Erick Daniel Perez Mata <erickdanielperezmata@outlook.es>
2023-12-31 02:04:17 -0600

    create counter view

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -4,11 +4,12 @@ import styles from "./button.module.scss";
 
 interface IButton {
   children: ReactNode;
+  onClick?: () => void;
 }
 
-export function Button({ children }: IButton) {
+export function Button({ children, onClick }: IButton) {
   return (
-    <button type="button" className={styles.button}>
+    <button type="button" className={styles.button} onClick={onClick}>
       {children}
     </button>
   );
--- src/views/counter/counter.tsx
+++ src/views/counter/counter.tsx
@@ -1,10 +1,28 @@
-import { Header } from "@src/components";
+import { useState } from "react";
+
+import { Header, Button } from "@src/components";
+
+import styles from "./counter.module.scss";
 
 export function Counter() {
+  const [count, setCount] = useState(0);
+
+  const handleDecrease = () => {
+    setCount((prevState) => prevState - 1);
+  };
+
+  const handleIncrease = () => {
+    setCount((prevState) => prevState + 1);
+  };
+
   return (
     <div>
       <Header />
-      <p>Counter</p>
+      <div className={styles.counter}>
+        <Button onClick={handleDecrease}>-1</Button>
+        <p className={styles.count}>{count}</p>
+        <Button onClick={handleIncrease}>+1</Button>
+      </div>
     </div>
   );
 }
